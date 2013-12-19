7e6495f3b177375b9e48ebd9dc585aadef5da682
oleggurduza <gurduza@mail.ru>
2013-12-19 08:55:31 +0200

    am adaugat inca o ruta p/u city helper

--- protected/config/main-router-frontend.php
+++ protected/config/main-router-frontend.php
@@ -2,6 +2,7 @@
 
 return array(
     '<lang:(ro|en)>' => 'site/default',
+	'<lang:(ro|en)>/city-helper' => 'site/cityhelper',
     '<lang:ro>/bilete-de-avion' => 'site/default',
     '<lang:en>/airtickets' => 'site/contact',
     '<lang:\w{2}>/<controller:\w+>/<action>'=>'<controller>/<action>',
